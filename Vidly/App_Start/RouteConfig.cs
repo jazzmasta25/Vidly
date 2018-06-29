164861de333a9aa119bb907f0dbbe1d0f9be70bd
Darren Rush <darrenrush@gmail.com>
2018-06-29 14:01:51 -0500

    Section 2

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,6 +12,19 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes(); //enabled attribute routes
+
+
+            /* Olden Days way
+            routes.MapRoute(
+                "MoviesByReleaseDate", //unique name
+                "movies/released/{year}/{month}", // url structure
+                new { controller = "Movies", action = "ByReleaseDate" }, //set the controller and action that will handle
+                new { year = @"2015|2016", month = @"\d{2}"} // set constraints on variables optionally
+                );
+            */
+
+
 
             routes.MapRoute(
                 name: "Default",
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+
+            return Content("");
+
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+
+
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")] //[Route("URL {PARAMETER:CONSTRAINTS()}")
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month.ToString("00"));
+        }
+
+        private List<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            };
+        }
+
+
+
+    }
+}
