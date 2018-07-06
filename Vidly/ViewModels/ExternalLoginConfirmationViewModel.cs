d1911bdbd34f85b916d086eac529bec60d886484
jazzmasta25 <darrenrush@gmail.com>
2018-07-06 14:54:22 -0500

    Install glimpse, organize classes

---
+++ Vidly/ViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.ViewModels
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Drivers License")]
+        public string DriversLicense { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Phone { get; set; }
+    }
+}
