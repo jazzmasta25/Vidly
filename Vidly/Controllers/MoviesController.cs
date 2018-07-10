9299311883d02fc4c2eb419e0ebe618e7256c3e9
jazzmasta25 <darrenrush@gmail.com>
2018-07-10 12:00:09 -0500

    Bulk upload option, headers validation

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -11,7 +11,7 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        private ApplicationDbContext _context; //database
+        private readonly ApplicationDbContext _context; //database
 
         public MoviesController() //constructor initialize database so we can use it
         {
@@ -26,7 +26,7 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movies() {Name = "Shrek!"};
+            var movie = new Movies {Name = "Shrek!"};
 
             return Content("");
 
@@ -49,14 +49,12 @@ namespace Vidly.Controllers
         [AllowAnonymous]
         public ActionResult Details(int id)
         {
-
             var movie = _context.Movies.Include(m => m.MovieGenre).SingleOrDefault(m => m.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
 
             return View(movie);
-
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")] //[Route("URL {PARAMETER:CONSTRAINTS()}")
@@ -69,13 +67,20 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var movieGenres = _context.MovieGenres.ToList();
-            var viewModel = new MovieFormViewModel()
+            var viewModel = new MovieFormViewModel
             {
                 Movies = new Movies(),
                 MovieGenres = movieGenres
             };
 
             return View("MovieForm", viewModel);
+        }
+
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public ActionResult Bulk()
+        {
+            var upload = new Upload {Processed = 0};
+            return View(upload);
         }
 
         [HttpPost]
@@ -85,7 +90,7 @@ namespace Vidly.Controllers
         {
             if (!ModelState.IsValid)
             {
-                var viewModel = new MovieFormViewModel()
+                var viewModel = new MovieFormViewModel
                 {
                     Movies = movies,
                     MovieGenres = _context.MovieGenres.ToList()
@@ -136,5 +141,91 @@ namespace Vidly.Controllers
             return View("MovieForm", viewModel);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public ActionResult BulkUpload(Upload upload)
+        {
+            if (!ModelState.IsValid) return View("Bulk");
+
+            var path = Server.MapPath("~/uploads/");
+
+            if (upload.File == null)
+                throw new NullReferenceException("File is null.");
+
+            if (upload.File != null)
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+            var uploadId = Path.GetFileNameWithoutExtension(Path.GetRandomFileName());
+            var extension = Path.GetExtension(upload.File.FileName);
+            var filePath = path + Path.GetFileNameWithoutExtension(upload.File.FileName) + "-" + uploadId + "-" +
+                           extension;
+
+            upload.File.SaveAs(filePath);
+
+            //CSV should have header row with name,stock,releaseDate,Genre
+
+            //read contents of file
+            var csvData = System.IO.File.ReadAllText(filePath);
+
+            var itemsProcessed = 0;
+
+            //loop over data
+            foreach (var row in csvData.Split('\n'))
+            {
+                var nameField = row.Split(',')[0];
+                var stockField = row.Split(',')[1];
+                var releaseDateField = row.Split(',')[2];
+                var genreField = row.Split(',')[3];
+                genreField = genreField.Replace("\r", "");
+
+                //validate headers
+                if (itemsProcessed == 0)
+                {
+                    if (nameField.ToLower() != "name") upload.ColumnError = "name";
+                    if (stockField.ToLower() != "stock") upload.ColumnError = "stock";
+                    if (releaseDateField.ToLower() != "releasedate") upload.ColumnError = "releaseDate";
+                    if (genreField.ToLower() != "genre") upload.ColumnError = "genre";
+                }
+
+                if (!string.IsNullOrWhiteSpace(upload.ColumnError))
+                {
+                    upload.Processed = 0;
+                    return View("Bulk", upload);
+                }
+
+                if (row.Split(',')[0].ToLower() == "name" && itemsProcessed == 0) //skip header row
+                {
+                    itemsProcessed++;
+                    continue;
+                } 
+
+                if (!string.IsNullOrEmpty(row))
+                {
+                    var movie = new Movies
+                    {
+                        Name = nameField,
+                        Stock = Convert.ToInt32(stockField),
+                        ReleaseDate = DateTime.Parse(releaseDateField),
+                        MovieGenreId = Convert.ToInt32(genreField),
+                        DateAdded = DateTime.Now
+                    };
+
+                    movie.NumberAvailable = movie.Stock;
+
+                    _context.Movies.Add(movie);
+                }
+
+                itemsProcessed++;
+            }
+
+            _context.SaveChanges();
+
+            upload.Processed = itemsProcessed - 1;
+
+            return View("Bulk", upload);
+        }
     }
+
 }
---
+++ Vidly/Models/Upload.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Upload
+    {
+        [Required(ErrorMessage = "Please select file.")]
+        public HttpPostedFileBase File { get; set; }
+
+        [FileExtensions(Extensions = "csv", ErrorMessage = "Only CSV Files allowed.")]
+        public string FileName
+        {
+            get
+            {
+                if (File != null)
+                    return File.FileName;
+                else
+                    return "";
+            }
+        }
+
+        public int Processed { get; set; }
+
+        public string ColumnError { get; set; }
+
+    }
+}
